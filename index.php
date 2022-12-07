4ce5762e7c8c59aba16ec05e1b9a3f0f75ff1071
Takkino31 <takkinoyaya@gmail.com>
2022-12-07 06:51:21 +0000

    Namespace and Full Qualify Name POO_2

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
 
-require_once('./Class/Cart.php');
+// require_once('./Class/Cart.php');
+require_once('./Class/Paypal/Payment.php');
+require_once('./Class/Stripe/Payment.php');
+require_once('./Class/Users/User.php');
 
-$cart = new Cart(2,200);
-$cart->setTotalPrice(100);
-$cart->discount(10);
-var_dump($cart->getTotalPrice());
+// $cart = new Cart(2,200);
+// $cart->setTotalPrice(100);
+// $cart->discount(10);
+// var_dump($cart->getTotalPrice());
+
+use \Class\Paypal\Payment;
+
+$paymentPaypal = new Payment();
+$paymentStripe = new Class\Stripe\Payment();
+
+
+var_dump($paymentPaypal);
+var_dump($paymentStripe);
